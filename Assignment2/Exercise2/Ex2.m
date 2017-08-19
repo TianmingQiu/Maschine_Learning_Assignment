75a393b88bda11d1c9b39f164840f66fdd1bcc48
TianmingQiu <qtianming@hotmail.com>
2017-08-19 09:08:47 +0200

    ex2

--- Assignment2/Exercise2/Ex2.m
+++ Assignment2/Exercise2/Ex2.m
@@ -1,3 +1,25 @@
 close all; clear; clc;
 load 'A.txt'; load 'B.txt'; load 'pi.txt'; load 'Test.txt';
+B = B';
+[T Samples] = size(Test);   % T: time step = 60 Samples: 10
 
+[N M] = size(B);    % M: obzavation = 8  N: State = 12
+
+%% Forward procedure
+alpha = zeros(T, N, Samples);
+for s = 1 : Samples
+    % Initialize
+    alpha(1, :, s) = pi' .* B(:, Test(1, s));
+    
+    % Induction
+    for t = 1 : T - 1
+        for j = 1 : N
+            alphasum = sum(alpha(t, :, s) .* A(:, j)');
+            alpha(t + 1, j, s) = alphasum * B(j, Test(t + 1, s));
+        end
+    end
+    
+    % Termination
+    p(s) = sum(alpha(T, :, s));
+end
+log_likelihood = log(p);
